45e9d6dd224d7afb4d8efb434313f1671a82cec2
James Boland <jamespatrickboland@gmail.com>
2023-02-04 11:26:27 +0000

    Added methods to linear_vector to convert simple formulas to linear_vectors and to create copies of linear vectors

--- 21/linear_vector.rs
+++ 21/linear_vector.rs
@@ -18,6 +18,10 @@ impl LinearVector {
         return Self {constant: constant, coeff: coeff, name: name.to_string()};
     }
 
+    pub fn create_copy(&self) -> Self {
+        return Self::new(self.constant, self.coeff, &self.name);
+    }
+
     pub fn to_formula(&self) -> Formula {
         if (self.coeff == 0) && (self.constant == 0) {
             return Formula::new(vec![Token::Constant(0)]);
@@ -33,35 +37,46 @@ impl LinearVector {
         }
     }
 
-    pub fn from_3_term_formula(formula: Formula, name: &String) -> Self {
-        if formula.get_variable_names().len() > 1 {panic!("Can't create a reduced formula when there is more than one variable unknown!");}
+    pub fn from_simple_formula(formula: Formula, name: &String) -> Result<Self, (String, Formula)> {
+        let formula_length = formula.get_formula().len();
+        return match formula_length {
+            1 => Self::from_1_term_formula(formula, name),
+            3 => Self::from_3_term_formula(formula, name),
+            2 => Err(("Formula malformed".to_string(), formula)),
+            _ => Err(("Formula is too long".to_string(), formula)),
+        };
+    }
+
+    fn from_3_term_formula(formula: Formula, name: &String) -> Result<Self, (String, Formula)> {
         if formula.get_formula().len() != 3 {panic!{"This method only handles 3 term formulae!"};}
+        if formula.get_variable_names().len() > 1 {return Err(("Too many unknowns to reduce".to_string(), formula));}
         if let Token::Op(operation) = formula.get_formula()[1] {
-            let term1 = Self::from_1_term_formula(Formula::new(vec![formula.get_formula()[0].create_copy()]), name);
-            let term2 = Self::from_1_term_formula(Formula::new(vec![formula.get_formula()[2].create_copy()]), name);
-            return match operation {
-                Operation::Addition => term1 + term2,
-                Operation::Subtraction => term1 - term2,
-                Operation::Multiplication => term1 * term2,
-                Operation::Division => term1 / term2,
-                Operation::Equals => panic!("We don't operate on formulae using =")
+            let result1 = Self::from_1_term_formula(Formula::new(vec![formula.get_formula()[0].create_copy()]), name);
+            let result2 = Self::from_1_term_formula(Formula::new(vec![formula.get_formula()[2].create_copy()]), name);
+            return match (operation, result1, result2) {
+                (Operation::Addition, Ok(term1), Ok(term2)) => Ok(term1 + term2),
+                (Operation::Subtraction, Ok(term1), Ok(term2)) => Ok(term1 - term2),
+                (Operation::Multiplication, Ok(term1), Ok(term2)) => Ok(term1 * term2),
+                (Operation::Division, Ok(term1), Ok(term2)) => Ok(term1 / term2),
+                (_, Err((reason, _)), _) | (_, _, Err((reason, _))) => Err(("Issue with one of the terms: ".to_string() + &reason, formula)),
+                (Operation::Equals, _, _) => Err(("Formula is an equation!".to_string(), formula)),
             };
         }
         else {
-            panic!("3-term formulae should have an operator");
+            return Err(("Formula is invalid!".to_string(), formula));
         }
 
     }
 
-    pub fn from_1_term_formula(formula: Formula, name: &String) -> Self {
+    fn from_1_term_formula(formula: Formula, name: &String) -> Result<Self, (String, Formula)> {
         let var_names = formula.get_variable_names();
         if var_names.len() > 1 {panic!("Can't create a reduced formula when there is more than one variable unknown!");}
         else if (var_names.len() == 1) && (var_names[0] != name.to_string()) {panic!("Variable name introduced is different from what was specified: {}, {}", name, var_names[0]);}
         if formula.get_formula().len() != 1 {panic!{"This method only handles 1 term formulae!"};}
         return match formula.get_formula()[0] {
-            Token::Constant(num) => Self::new(num, 0, name),
-            Token::Term(num, _) => Self::new(0, num, name),
-            Token::Variable(_) => Self::new(0, 1, name),
+            Token::Constant(num) => Ok(Self::new(num, 0, name)),
+            Token::Term(num, _) => Ok(Self::new(0, num, name)),
+            Token::Variable(_) => Ok(Self::new(0, 1, name)),
             Token::Op(_) => panic!("Can't create a reduced formula from an operation"),
         };
     }
