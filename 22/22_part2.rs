17c89bfcda51dcf9d8c164c0655b64c51520a7ba
James Boland <jamespatrickboland@gmail.com>
2023-02-11 02:13:01 +0000

    Algorithm for part 2 working but producing the wrong answer

---
+++ 22/22_part2.rs
@@ -0,0 +1,140 @@
+use std::env;
+use std::fs;
+use std::collections::{HashMap,HashSet,VecDeque};
+
+mod space;
+use space::{Direction,Marker,Point,Rotation,StdInt};
+mod space3d;
+use space3d::{Direction3D,Orientation};
+mod face;
+use face::EdgeGlue;
+mod map;
+use map::Map;
+mod reading_input;
+use reading_input::get_input_data;
+
+fn main() {
+    let env_args: Vec<String> = env::args().collect();
+    let file_name = &env_args[1];
+    let face_size: StdInt = env_args[2].parse().unwrap();
+    println!("File name is '{}'. Reading input...", file_name);
+    let input = fs::read_to_string(file_name).expect("Should have been able to read the file"); 
+    let (unglued_map, instructions, mut current_marker): (Map, Vec<String>, Marker) = get_input_data(input, face_size);
+    let map = glue_faces(&unglued_map);
+
+    let total_instructions = &instructions.len();
+    println!("Initial state: {}, num instructions: {}", current_marker, &total_instructions);
+    println!("Data loaded. Traversing map...");
+
+    for instruction in instructions.iter() {
+        current_marker = match instruction.parse::<StdInt>() {
+            Ok(distance) => map.get_new_position(&current_marker, distance),
+            Err(_) => current_marker.get_rotated_marker(&Rotation::from_string(&instruction)),
+        };
+    }
+    println!("Map traversed");
+    println!("Final marker: {}", &current_marker);
+    let current_point: Point = current_marker.get_position();
+    let current_direction: Direction = current_marker.get_direction();
+    let password: StdInt = (1000 * current_point.y) + (4 * current_point.x) + current_direction.as_int();
+    println!("Password is {}", password);
+} 
+
+
+fn glue_faces(old_map: &Map) -> Map {
+    let mut map = old_map.create_copy();
+    println!("Glueing faces...");
+    let max_face = map.find_face(&Point::new(map.get_max_x().unwrap(), map.get_max_y().unwrap()));
+    let max_x = max_face.x;
+    let max_y = max_face.y;
+    for j in 1..=max_y {
+        for i in 1..=max_x {
+            let this_face: Point = Point::new(i, j);
+            if !map.has_face(&this_face) || map.is_face_fully_glued(&this_face) {continue;}
+            let neighbours: HashMap<Direction,EdgeGlue> = find_neighbours(&map, &this_face);
+            for (direction, (neighbour, rotation)) in neighbours.iter() {
+                map.bidirectional_glue_faces(&this_face, &neighbour, &direction, &rotation);
+            }
+        }
+    }
+    return map;
+}
+
+
+fn find_neighbours(map: &Map, start: &Point) -> HashMap<Direction, EdgeGlue> {
+    let (previous_in_path, d3d_to_face) = dfs_for_rotations(map, start);
+
+    let mut edge_glues: HashMap<Direction, EdgeGlue> = HashMap::new();
+    for direction in map.get_faces_unglued_directions(start) {
+        let direction3d: Direction3D = Direction3D::from2d_as_face_direction_rel_z(&direction);
+        let face: Point = *d3d_to_face.get(&direction3d).unwrap();
+
+        let new_tangent = get_new_tangent(start, &face, &direction3d, &previous_in_path);
+        let current_tangent: Direction3D = Direction3D::from2d_as_face_direction_rel_z(&direction);
+        let tangent_rotation: Rotation = Direction3D::get_2d_rotation_from_tangent_change_on_z_face(&current_tangent, &new_tangent);
+        edge_glues.insert(direction, (face, tangent_rotation.inverse()));
+    }
+    return edge_glues;
+}
+
+fn dfs_for_rotations(map: &Map, start: &Point) 
+    -> (HashMap<Point,(Point,Direction3D)>, HashMap<Direction3D,Point>) {    
+    let mut from_previous: HashMap<Point,(Point,Direction3D)> = HashMap::new();
+    let mut orientation_from_face: HashMap<Point,Orientation> = HashMap::new();
+    let mut d3d_to_face: HashMap<Direction3D,Point> = HashMap::from([(Direction3D::Z, *start)]);
+
+    let mut queue: VecDeque<Point> = VecDeque::new();
+    let mut queued: HashSet<Point> = HashSet::new();
+    let mut explored: HashSet<Point> = HashSet::new();
+    let mut current_face: Point = *start;
+    let mut current_orientation: Orientation = Orientation::new();
+
+    loop {
+        let next_faces: Vec<(Point,Direction)> = get_flat_neighbour_faces(map, &current_face);
+        for (face, direction) in next_faces {
+            if !queued.contains(&face) && !explored.contains(&face) {
+                let new_rotation = current_orientation.get_rotation_axis_for_direction(&direction);
+                let new_orientation = current_orientation.rotate(&new_rotation);
+                orientation_from_face.insert(face, new_orientation);
+                d3d_to_face.insert(new_orientation.get_face_direction(), face);
+                from_previous.insert(face, (current_face, new_rotation));
+                queued.insert(face);
+                queue.push_back(face);
+            }
+        }
+        explored.insert(current_face);
+
+        if (explored.len() == 6) || (queue.len() == 0) {
+            break;
+        }
+        current_face = queue.pop_front().unwrap();
+        current_orientation = *orientation_from_face.get(&current_face).unwrap();
+    }
+    return (from_previous, d3d_to_face);
+}
+
+fn get_flat_neighbour_faces(map: &Map, current_face: &Point) -> Vec<(Point, Direction)> {
+    let mut neighbours: Vec<(Point, Direction)> = Vec::new();
+    for direction in Direction::get_directions() {
+        let new_flat_position = *current_face + direction.as_vector();
+        if map.has_face(&new_flat_position) {
+            neighbours.push((new_flat_position, direction));
+        }
+    }
+    return neighbours;
+}
+
+fn get_new_tangent(start: &Point, target: &Point, target_as_3d: &Direction3D, previous_steps: &HashMap<Point,(Point,Direction3D)>) -> Direction3D {
+    let mut rotations: Vec<Direction3D> = Vec::new();
+    let mut current_face = target;
+    while current_face != start {
+        let (prev_face, rotation) = previous_steps.get(&current_face).unwrap();
+        rotations.push(*rotation);
+        current_face = prev_face;
+    }
+    let mut new_tangent: Direction3D = *target_as_3d;
+    for rotation in rotations.iter().rev() {
+        new_tangent = new_tangent.rotate_around(&rotation);
+    }
+    return new_tangent;
+}
--- 22/space3d.rs
+++ 22/space3d.rs
@@ -1,31 +1,31 @@
-use std::ops::{Mul,Div,Neg};
-
-use space::{StdInt,Direction,Rotation};
+use std::ops::{Mul,Neg};
+use space::{Direction,Rotation};
 
 #[derive(Debug,Copy,Clone,Hash,PartialEq,Eq)]
-enum Direction3D {
+pub enum Direction3D {
     X,
     NegX,
     Y,
     NegY,
     Z,
     NegZ,
+    Zero,
 }
 
 impl Direction3D {
-    pub fn rotate_around(&self, axis: Self) -> Self {
-        if (axis == self) || (axis == -self) {
-            return axis;
+    pub fn rotate_around(&self, axis: &Self) -> Self {
+        if (axis == self) || (*axis == self.neg()) {
+            return *axis;
         }
-        return axis * self;
+        return *axis * *self;
     }
 
     pub fn from2d_as_rotation_axis_rel_z(direction: &Direction) -> Self {
         return match direction {
             Direction::Up => Self::NegX,
             Direction::Down => Self::X,
-            Direction::Right => Self::NegX,
-            Direction::Left => Self::X,
+            Direction::Right => Self::Y,
+            Direction::Left => Self::NegY,
         };
 
     }
@@ -39,35 +39,34 @@ impl Direction3D {
         };
     }
 
-    pub fn get_2d_rotation_from_normal_change_on_z_face(start: &Direction3D, end: &Direction3D) {
-        return match (start, end) {
-            (Self::Z, _) => panic!("Z shouldn't be a starting normal"),
-            (Self::NegZ, _) => panic!("-Z shouldn't be a starting normal"),
+    pub fn get_2d_rotation_from_tangent_change_on_z_face(start: &Direction3D, end: &Direction3D) -> Rotation {
+        let rotation = match (start, end) {
             (_, Self::NegZ) => Rotation::None,
             (_, Self::Z) => Rotation::Half,
             (Self::Y, Self::NegX) | (Self::NegX, Self::NegY) | (Self::NegY, Self::X) | (Self::X,Self::Y) => Rotation::Left,
             (Self::NegY, Self::NegX) | (Self::X, Self::NegY) | (Self::Y, Self::X) | (Self::NegX,Self::Y) => Rotation::Right,
-            (Self::Y,Self::Y) | (Self::X, Self::X) | (Self::NegY, Self::NegY) | (Self::NegX, Self::NegX) => Rogation::None,
-            (Self::Y,Self::NegY) | (Self::X, Self::NegX) | (Self::NegY, Self::Y) | (Self::NegX, Self::X) => Rogation::Half,
+            (Self::Y,Self::Y) | (Self::X, Self::X) | (Self::NegY, Self::NegY) | (Self::NegX, Self::NegX) => Rotation::None,
+            (Self::Y,Self::NegY) | (Self::X, Self::NegX) | (Self::NegY, Self::Y) | (Self::NegX, Self::X) => Rotation::Half,
+            (Self::Z, _) => panic!("Z shouldn't be a starting tangent!"),
+            (Self::NegZ, _) => panic!("-Z shouldn't be a starting tangent!"),
+            (Self::Zero, _) | (_, Self::Zero) => panic!("Zero should never be a tangent!")
         };
+        return rotation;
     }
-
-    // Steps: 1. dfs for faces on 2d surface, noting the axis rotation we move along (by converting from the direction to axis of rotation)
-    // 2. When we note that we've found a face that should be glued to our start face: Apply those axis rotations for the relevant normal 
-    //     (if we find the left side, NegX) to get the "new orientation" of the normal
-    // 3. Get the direction of normal change and invert it to get the direction needed to fix it (giving us glue rotatoin)
 }
 
 impl Mul for Direction3D { 
     type Output = Self;
     fn mul(self, other: Self) -> Self {
         return match (self, other) {
-            (Self::X, Self::Y) | (Self::NegY, Self::X) | (Self::Y, Self::NegX) | (Self::NegX, Self::NegY) => (Self::Z),
-            (Self::NegX, Self::Y) | (Self::Y, Self::X) | (Self::NegY, Self::NegX) | (Self::X, Self::NegY) => (Self::NegZ),
-            (Self::Y, Self::Z) | (Self::NegZ, Self::Y) | (Self::Z, Self::NegY) | (Self::NegY, Self::NegZ) => (Self::X),
-            (Self::NegY, Self::Z) | (Self::Z, Self::Y) | (Self::NegZ, Self::NegY) | (Self::Y, Self::NegZ) => (Self::NegX),
-            (Self::Z, Self::X) | (Self::NegX, Self::Z) | (Self::X, Self::NegZ) | (Self::NegZ, Self::NegX) => (Self::Y),
-            (Self::NegZ, Self::X) | (Self::X, Self::Z) | (Self::NegX, Self::NegZ) | (Self::Z, Self::NegX) => (Self::NegY),
+            (Self::X, Self::Y) | (Self::NegY, Self::X) | (Self::Y, Self::NegX) | (Self::NegX, Self::NegY) => Self::Z,
+            (Self::NegX, Self::Y) | (Self::Y, Self::X) | (Self::NegY, Self::NegX) | (Self::X, Self::NegY) => Self::NegZ,
+            (Self::Y, Self::Z) | (Self::NegZ, Self::Y) | (Self::Z, Self::NegY) | (Self::NegY, Self::NegZ) => Self::X,
+            (Self::NegY, Self::Z) | (Self::Z, Self::Y) | (Self::NegZ, Self::NegY) | (Self::Y, Self::NegZ) => Self::NegX,
+            (Self::Z, Self::X) | (Self::NegX, Self::Z) | (Self::X, Self::NegZ) | (Self::NegZ, Self::NegX) => Self::Y,
+            (Self::NegZ, Self::X) | (Self::X, Self::Z) | (Self::NegX, Self::NegZ) | (Self::Z, Self::NegX) => Self::NegY,
+            (Self::Zero, _) | (_, Self::Zero) => Self::Zero,
+            (a, b) => if (a == b) || (a == b.neg()) {Self::Zero} else {panic!("Unexpected combination for cross product: {:?}, {:?}", a, b)},
         }
     }
 }
@@ -82,7 +81,53 @@ impl Neg for Direction3D {
             Self::NegY => Self::Y,
             Self::Z => Self::NegZ,
             Self::NegZ => Self::Z,
+            Self::Zero => Self::Zero,
         }
+    }
+}
+
+
+#[derive(Debug,Copy,Clone)]
+pub struct Orientation {
+    face: Direction3D,
+    left: Direction3D,
+    right: Direction3D,
+    up: Direction3D,
+    down: Direction3D,
+}
+
+impl Orientation {
+    pub fn new() -> Self {
+        return Self{
+            face: Direction3D::Z,
+            left: Direction3D::from2d_as_rotation_axis_rel_z(&Direction::Left),
+            right: Direction3D::from2d_as_rotation_axis_rel_z(&Direction::Right),
+            up: Direction3D::from2d_as_rotation_axis_rel_z(&Direction::Up),
+            down: Direction3D::from2d_as_rotation_axis_rel_z(&Direction::Down),
+        }
+    }
+
+    pub fn rotate(&self, rotation_axis: &Direction3D) -> Self {
+        return Self{
+            face: self.face.rotate_around(rotation_axis),
+            left: self.left.rotate_around(rotation_axis),
+            right: self.right.rotate_around(rotation_axis),
+            up: self.up.rotate_around(rotation_axis),
+            down: self.down.rotate_around(rotation_axis),
+        }
+    }
+
+    pub fn get_rotation_axis_for_direction(&self, direction: &Direction) -> Direction3D {
+        return match direction {
+            Direction::Up => self.up,
+            Direction::Down => self.down,
+            Direction::Left => self.left,
+            Direction::Right => self.right,
+        };
+    }
+
+    pub fn get_face_direction(&self) -> Direction3D {
+        return self.face;
     }
 }
 
