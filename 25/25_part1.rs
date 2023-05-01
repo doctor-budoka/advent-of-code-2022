41dd60ecec8e1463890605da86128d7bffdcf6ac
James Boland <jamespatrickboland@gmail.com>
2023-05-01 19:27:55 +0200

    We now convert decimal total back to a base 5 string

--- 25/25_part1.rs
+++ 25/25_part1.rs
@@ -15,6 +15,8 @@ fn main() {
     }
 
     println!("Total in decimal: {}", total);
+    let total_snafu: String = decimal_to_snafu(total);
+    println!("Total in SNAFU: {}", total_snafu);
 } 
 
 fn snafu_to_decimal(line: &String) -> i32 {
@@ -34,4 +36,26 @@ fn snafu_digit_to_decimal(digit: char) -> i32 {
         '-' => -1,
         a => a.to_digit(10).expect("Should have been able to parse input") as i32,
     };
+}
+
+fn decimal_to_snafu(decimal: i32) -> String {
+    let base5: String = decimal_to_base5(decimal);
+    return base5_to_snafu(base5);
+}
+
+fn decimal_to_base5(decimal: i32) -> String {
+    let length: usize = (decimal as f32).log(5.0) as usize + 1;
+    let mut out_vec: Vec<i32> = Vec::new();
+    let mut leftover: i32 = decimal;
+    for i in (0..length).rev() {
+        let divisor = 5_i32.pow(i as u32);
+        let new_digit = leftover / divisor;
+        leftover -= new_digit * divisor;
+        out_vec.push(new_digit);
+    }
+    return out_vec.iter().map(|x| char::from_digit((*x) as u32, 5).unwrap()).collect::<String>();
+}
+
+fn base5_to_snafu(base5: String) -> String {
+    return base5;
 }
